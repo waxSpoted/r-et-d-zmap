c1e9214ecc0e076cb6112ce86156b8ce51e03771
wax222 <107887640+waxSpoted@users.noreply.github.com>
2024-01-26 11:35:49 +0100

    Update scan-ports-zmap.sh for scan 4 machines

--- hote/scan-ports-zmap.sh
+++ hote/scan-ports-zmap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-liste_ip_default="192.168.159.138 192.168.159.139"
+liste_2ip_default="192.168.159.138 192.168.159.139"
 liste_3ip_default="192.168.159.138 192.168.159.139 192.168.159.140"
+liste_4ip_default="192.168.159.138 192.168.159.139 192.168.159.140 192.168.159.131"
 single_ip_default="192.168.159.138"
 ports_default="1-33767"
 
@@ -48,10 +49,10 @@ function scan_deux_machines(){
 	if [ $default == "Y" ] || [ $default == "y" ]
 	then 
 		sudo echo ""
-		ip1=$(echo $liste_ip_default | tr ' ' '\n' | sed -n '1p')
-		ip2=$(echo $liste_ip_default | tr ' ' '\n' | sed -n '2p')
-		#ip1=$(echo $liste_ip_default | cut -c-15)
-		#ip2=$(echo $liste_ip_default | cut -c16-31)
+		ip1=$(echo $liste_2ip_default | tr ' ' '\n' | sed -n '1p')
+		ip2=$(echo $liste_2ip_default | tr ' ' '\n' | sed -n '2p')
+		#ip1=$(echo $liste_2ip_default | cut -c-15)
+		#ip2=$(echo $liste_2ip_default | cut -c16-31)
 		#echo "ip 1 : $ip1"
 		#echo "ip 2 : $ip2"
 		time sudo zmap --output-module=csv --output-fields=sport --output-filter="" --no-header-row -p $ports_default $ip1 >> ./file/output.csv &
@@ -141,6 +142,66 @@ function scan_trois_machines(){
 	echo "vous pouvez retrouver les ports ouverts dans ./file/output.csv"
 }
 
+function scan_quatre_machines(){
+	if [ -f ./file/output.csv ]
+	then
+		echo "" > ./file/output.csv
+	fi
+	echo "voulez vous utiliser la liste d'ip par défaut ? Y/n" 
+	read default 
+	if [ $default == "Y" ] || [ $default == "y" ]
+	then 
+		sudo echo ""
+		ip1=$(echo $liste_4ip_default | tr ' ' '\n' | sed -n '1p')
+		ip2=$(echo $liste_4ip_default | tr ' ' '\n' | sed -n '2p')
+		ip3=$(echo $liste_4ip_default | tr ' ' '\n' | sed -n '3p')
+		ip4=$(echo $liste_4ip_default | tr ' ' '\n' | sed -n '4p')
+		#ip1=$(echo $liste_ip_default | cut -c-15)
+		#ip2=$(echo $liste_ip_default | cut -c16-31)
+#		echo "ip 1 : $ip1"
+#		echo "ip 2 : $ip2"
+#		echo "ip 3 : $ip3"
+#		echo "ip 4 : $ip4"
+		time sudo zmap --output-module=csv --output-fields=sport --output-filter="" --no-header-row -p $ports_default $ip1 >> ./file/output.csv &
+		time sudo zmap --output-module=csv --output-fields=sport --output-filter="" --no-header-row -p $ports_default $ip2 >> ./file/output.csv &
+		time sudo zmap --output-module=csv --output-fields=sport --output-filter="" --no-header-row -p $ports_default $ip3 >> ./file/output.csv &
+		time sudo zmap --output-module=csv --output-fields=sport --output-filter="" --no-header-row -p $ports_default $ip4 >> ./file/output.csv &
+	else 
+		liste=""
+		for (( i=1; i<= 4; i++ ))
+		do
+			echo "Quelle est l'ip de la machine $i ?"
+			read ip
+			liste="$liste $ip"
+		done
+		echo "Quels sont les ports que vous voulez scanner ? 1-xxxx"
+		read ports
+		#echo "liste : $liste"
+		ip1=$(echo $liste | tr ' ' '\n' | sed -n '1p')
+		ip2=$(echo $liste | tr ' ' '\n' | sed -n '2p')
+		ip3=$(echo $liste | tr ' ' '\n' | sed -n '3p')
+		ip4=$(echo $liste | tr ' ' '\n' | sed -n '4p')
+		echo "exécution du scan :"
+		sudo echo ""
+#		echo "ip 1 : $ip1"
+#		echo "ip 2 : $ip2"
+#		echo "ip 3 : $ip3"
+#		echo "ip 4 : $ip4"
+		time sudo zmap --output-module=csv --output-fields=sport --output-filter="" --no-header-row -p $ports $ip1 >> ./file/output.csv &
+		time sudo zmap --output-module=csv --output-fields=sport --output-filter="" --no-header-row -p $ports $ip2 >> ./file/output.csv &
+		time sudo zmap --output-module=csv --output-fields=sport --output-filter="" --no-header-row -p $ports $ip3 >> ./file/output.csv &
+		time sudo zmap --output-module=csv --output-fields=sport --output-filter="" --no-header-row -p $ports $ip4 >> ./file/output.csv &
+#		echo ${liste[@]} | parallel time sudo zmap --output-module=csv --output-fields=sport --output-filter=\"\" --no-header-row -p $ports {} > ports.csv
+#		echo ${liste[1]} | parallel time sudo zmap --output-module=csv --output-fields=sport --output-filter=\"\" --no-header-row -p $ports {} > ports.csv
+	
+#		for i in ${liste[@]}
+#		do 
+#			#$(time sudo zmap --output-module=csv --output-fields=sport --output-filter="" --no-header-row -p $ports $i >> ./file/output.csv &)
+#			echo "ip : $i"
+#		done
+	fi
+	echo "vous pouvez retrouver les ports ouverts dans ./file/output.csv"
+}
 
 function scan_classique(){
 	echo ""
@@ -152,6 +213,7 @@ function scan_classique(){
 		echo "1 : 1 machine"
 		echo "2 : 2 machines"
 	       	echo "3 : 3 machines" 	
+		echo "4 : 4 machines"
 		read choix 
 		if [ $choix == 1 ]
 		then 
@@ -169,7 +231,7 @@ function scan_classique(){
 		if [ $choix == 2 ]
 		then 	
 			sudo echo ""
-			time sudo zmap --output-module=csv --output-fields=sport --output-filter="" --no-header-row -p $ports_default $liste_ip_default > ./file/output.csv 
+			time sudo zmap --output-module=csv --output-fields=sport --output-filter="" --no-header-row -p $ports_default $liste_2ip_default > ./file/output.csv 
 			nb_port=$(wc -l ./file/output.csv | tr ' ' '\n' | sed -n '1p')
 			if [ $nb_port == 1000 ]
 			then
@@ -192,6 +254,19 @@ function scan_classique(){
 			fi
 			echo "vous pouvez voir les ports ouverts dans ./file/output.csv"
 		fi 
+		if [ $choix == 4 ]
+		then 	
+			sudo echo ""
+			time sudo zmap --output-module=csv --output-fields=sport --output-filter="" --no-header-row -p $ports_default $liste_4ip_default > ./file/output.csv 
+			nb_port=$(wc -l ./file/output.csv | tr ' ' '\n' | sed -n '1p')
+			if [ $nb_port == 2000 ]
+			then
+				echo "tous les ports ont été découvert"
+			else
+				echo "tous les ports n'ont pas été découvert"
+			fi
+			echo "vous pouvez voir les ports ouverts dans ./file/output.csv"
+		fi 
 	else 
 		echo "Veuillez entrer l'ip ou les ip : "
 		read ip 
@@ -207,7 +282,8 @@ function scan_classique(){
 echo "1 : scanner une machine		[parallel]"
 echo "2 : scanner deux machines	[parallel]"
 echo "3 : scanner trois machines 	[parallel]"
-echo "4 : scan classique"
+echo "4 : scanner quatre machines 	[parallel]"
+echo "5 : scan classique"
 read choix 
 
 if [ $choix == 1 ]
@@ -226,6 +302,11 @@ then
 fi
 
 if [ $choix == 4 ]
+then
+	scan_quatre_machines
+fi
+
+if [ $choix == 5 ]
 then
 	scan_classique
 fi
